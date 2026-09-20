c6a97cb159c7877f72dbc96208debd0b7f1ec353
agustinhttps1913 <agusteen1881@gmail.com>
2026-09-20 11:33:55 -0300

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  // Relative assets make the build work both at user.github.io and /repo-name/.
-  base: './',
-})
